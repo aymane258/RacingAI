9250db8ee9dacbd454af974b472972e96e279afa
Marnixvdr <32701626+Marnixvdr@users.noreply.github.com>
2020-12-13 19:50:54 +0100

    rewards

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Checkpoint")
+        {
+            AddReward(1.0f);
+        }
+        else if (collision.collider.tag == "Wall")
+        {
+            AddReward(-0.1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using System.Collections;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    private TextMeshPro scoreBoard;
+
+    private Character character;
+
+    public void OnEnable()
+    {
+        car = transform.GetComponentInChildren<Car>();
+        scoreBoard = transform.GetComponentInChildren<TextMeshPro>();
+    }
+
+    private void FixedUpdate()
+    {
+        scoreBoard.text = character.GetCumulativeReward().ToString("f2");
+    }
+}
