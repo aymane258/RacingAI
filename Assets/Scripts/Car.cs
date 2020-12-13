0c8d164cd5001da4939324fc4502c3c69f1e9c4c
Marnixvdr <32701626+Marnixvdr@users.noreply.github.com>
2020-12-13 21:22:50 +0100

    Update Car.cs

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
 
-public class Car : MonoBehaviour
+public class Car : Agent
 {
     // Start is called before the first frame update
     void Start()
