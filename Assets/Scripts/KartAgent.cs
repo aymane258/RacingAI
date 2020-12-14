b91badf09e4359a6353926de8c45d6b70a1c6492
aymane258 <aymane-abdellah@hotmail.com>
2020-12-14 08:14:08 +0100

    :+1:

---
+++ Assets/Scripts/KartAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class KartAgent : Agent
+{
+
+   private KartController _kartController;
+   
+
+   public override void Initialize()
+   {
+      _kartController = GetComponent<KartController>();
+   }
+   
+
+   public override void OnEpisodeBegin()
+   {
+
+      _kartController.Respawn();
+   }
+
+      public override void OnActionReceived(ActionBuffers actions)
+      {
+        var input = actions.ContinuousActions;
+
+        _kartController.ApplyAcceleration(input[1]);
+        _kartController.Steer(input[0]);
+      }
+      
+      //manueel
+      public override void Heuristic(in ActionBuffers actionsOut)
+      {
+        var action = actionsOut.ContinuousActions;
+        action[0] = Input.GetAxis("Horizontal");
+        action[1] = Input.GetKey(KeyCode.W) ? 1f : 0f;
+      }
+
+}
