1b2bfbad44d0a61e0ebbb01e271de0be12d095e5
Younes Hlalem <s109442@ap.be>
2020-12-23 12:39:36 +0100

    Stuur opstelling

---
+++ Assets/Scripts/SteeringControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringControl : MonoBehaviour
+{
+    public GameObject rHand;
+    private Transform rHandParent;
+    private bool rHandOnWheel = false;
+
+    public GameObject lHand;
+    private Transform lHandParent;
+    private bool lHandOnWheel = false;
+
+    public Transform[] snappoints;
+
+    public GameObject kart;
+    public Rigidbody kartRB;
+
+    public float currentWheelRotation = 0;
+
+    private float turnDampening = 5000;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
